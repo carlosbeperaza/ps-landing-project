e100ce4246b7575550f3c4d63193e1d2203aafcb
Laura Chan <laurachan000@gmail.com>
2020-03-28 23:25:50 -0500

    UserData finished, i can't prove it

--- src/main/java/com/ps/landing/project/converters/UserDataConverter.java
+++ src/main/java/com/ps/landing/project/converters/UserDataConverter.java
@@ -1,5 +1,6 @@
 package com.ps.landing.project.converters;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,31 +9,27 @@ import org.springframework.stereotype.Component;
 
 import com.ps.landing.project.dto.UserDataDTO;
 import com.ps.landing.project.models.PSUser;
+import com.ps.landing.project.models.Role;
+import com.ps.landing.project.models.Module;
+
 
 @Component("UserDataConverter")
 public class UserDataConverter {
 	
 	@Autowired
 	private ModuleConverter moduleConverter;
-	
-	public PSUser UserDataDTOtoUser(UserDataDTO userDataDTO) {
-
-		PSUser user = new PSUser();
-
 		
-		user.setName(userDataDTO.getName());
-		user.setLastname(userDataDTO.getLastname());
-		user.setEmail(userDataDTO.getEmail());
-		user.setRegistrationDate(userDataDTO.getRegistrationDate());
-		user.setUpdateDate(userDataDTO.getUpdateDate());
-		user.setRoles(moduleConverter.convertToModel(userDataDTO.getModules()));
-
-		return user;
-	}
-	
 	public UserDataDTO UsertoUserDataDTO(PSUser user) {
 
 		UserDataDTO userDTO = new UserDataDTO();
+		List<Module> listaModules = new ArrayList<>();
+		
+		
+		for(Role role:user.getRoles()) {
+			for(Module module:role.getModules()) {
+				listaModules.add(module);
+			}
+		}
 
 		
 		userDTO.setName(user.getName());
@@ -40,23 +37,14 @@ public class UserDataConverter {
 		userDTO.setEmail(user.getEmail());
 		userDTO.setRegistrationDate(user.getRegistrationDate());
 		userDTO.setUpdateDate(user.getUpdateDate());
-		userDTO.setModules(ModuleConverter.convertToDTO(user.getModules()));
+		userDTO.setModules(moduleConverter.convertToDTO(listaModules));
+		
+		
 		return userDTO;
 
 	}
 	
-	public List<UserDataDTO> convertToDTO(List<PSUser> users) {
-
-		List<UserDataDTO> userDTOS = new ArrayList<>();
-
-		for (PSUser user : users) {
-
-			UserDataDTO userDTO = UsertoUserDataDTO(user);
-			userDTOS.add(userDTO);
-		}
-
-		return userDTOS;
-	}
+	
 
 
 }
--- src/main/java/com/ps/landing/project/servicesImpls/UserDataServiceImpl.java
+++ src/main/java/com/ps/landing/project/servicesImpls/UserDataServiceImpl.java
@@ -4,23 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ps.landing.project.converters.UserDataConverter;
 import com.ps.landing.project.dto.UserDataDTO;
-import com.ps.landing.project.repos.UserDataRepo;
+import com.ps.landing.project.models.PSUser;
+import com.ps.landing.project.repos.UserRepo;
 import com.ps.landing.project.services.UserDataService;
 
 
 public class UserDataServiceImpl implements UserDataService {
 
 	@Autowired
-	private UserDataRepo userRepo;
-
+	private UserRepo userRepo;
+	
 	@Autowired
 	private UserDataConverter userconverter;
 	@Override
 	public UserDataDTO findByUsername(String username) {
 		
-		UserDataDTO user = userRepo.findByUsername(username).orElse(null);
+		 PSUser user = userRepo.findByUsername(username).orElse(null);
 
-		return (user != null) ? userconverter.UsertoUserDataDTO(user) : null;
+		 return (user != null) ? userconverter.UsertoUserDataDTO(user) : null;
 	}
 	
 	
