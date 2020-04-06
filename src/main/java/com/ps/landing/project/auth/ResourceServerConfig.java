86bf9758211062273842b022f526eda5ae88aa26
Alejandro-Abdul-Jimenez-Martinez <59883781+Alejandro-Abdul-Jimenez-Martinez@users.noreply.github.com>
2020-04-06 15:47:41 -0500

    change '/user/forgot-password/' to .permitAll()

--- src/main/java/com/ps/landing/project/auth/ResourceServerConfig.java
+++ src/main/java/com/ps/landing/project/auth/ResourceServerConfig.java
@@ -23,6 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/base/**").authenticated()
+                .antMatchers("/user/forgot-password").permitAll()
 		/*.antMatchers(HttpMethod.GET, "/api/clientes/{id}").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
 		.antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
