acd9ef662c92aff52b5c12e207d82d173cd1237e
Alejandro-Abdul-Jimenez-Martinez <59883781+Alejandro-Abdul-Jimenez-Martinez@users.noreply.github.com>
2020-04-17 00:39:31 -0500

    Implement of integrity validations in save() method for RoleServiceImpl.java (in testing)

--- src/main/java/com/ps/landing/project/repos/RoleRepo.java
+++ src/main/java/com/ps/landing/project/repos/RoleRepo.java
@@ -3,6 +3,8 @@ package com.ps.landing.project.repos;
 import com.ps.landing.project.models.Catalog;
 import com.ps.landing.project.models.Role;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.ps.landing.project.models.Role;
 
